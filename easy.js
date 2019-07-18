fd8161379d617b0b9e96716ebd7076b097b573b5
PiyushSaini12 <43487064+PiyushSaini12@users.noreply.github.com>
2019-07-17 22:15:44 -0400

    Add files via upload

--- easy.js
+++ easy.js
@@ -7,6 +7,9 @@ var result = false;
 // I suggest using an array of objects but feel free to change that
 // An example of a puzzle piece object could be: { name: ".box1", x: 0, y: 0 }
 **********************************/
+
+//declaring the x and y co-ordinates so the base gets ready
+
 const puzzlePieces = [
   { name: ".box1", x:0, y:0},
   { name: ".box2", x:100, y:0},
@@ -31,12 +34,15 @@ const puzzlePieces = [
 const blankSpace = { x: 300, y: 300, order: 16 };
 
 // I'm structuring my program sort of like how Vue does it - all in my puzzle object below.
+
+//declaring an object called puzzle
 const puzzle = {
   pieces: puzzlePieces,
   distance: DISTANCE,
   blankSpace,
   currentPiece: null,
   directionToMove: "",
+  //initialize funtion to start the game
   initialize: function() {
 
     /************************************     
@@ -50,6 +56,7 @@ const puzzle = {
     this.display();
   },
 
+  //this function right here displays what we've defined above
   display: function() {
     // initialize pieces to their proper order
     this.pieces.forEach(piece => {
@@ -57,6 +64,10 @@ const puzzle = {
       TweenLite.set(pieceDOM, { x: piece.x, y: piece.y });
     });
   },
+
+
+  //slide function to actually move the puzzle pieces 
+  //also passing an event to it to focus on the div tag on which the user has clicked on
 
   slide: function(e) {
     // call isMoveable to find out direction to move
@@ -69,6 +80,7 @@ const puzzle = {
     puzzle.thirdPiece = puzzle.pieces[e.target.dataset.idx - 2];
     directionToMove = puzzle.isMoveable();
 
+    console.log(this);
     // Now animate current puzzle piece now that x, y coordinates have been set above
     
   
@@ -78,6 +90,7 @@ const puzzle = {
     //    ease: Power0.easeNone
     //  });
 
+    //code to move the puzzle piece
     if((directionToMove=="up-down")||(directionToMove=="left-right")){
        TweenMax.to(this, 0.17, {
        x: puzzle.currentPiece.x,//blankSpace.x,
@@ -88,16 +101,21 @@ const puzzle = {
     
 
   },
+
+  //this is the function that checks the direction and sees if the cube is moving in x or in y 
+
   isMoveable: function() {
     /********************************************
     // STEP 3 - Implement isMoveable function to find out / return which direction to move
     // Is the clicked piece movable?
     // If yes, then return a direction to one of: "up", "down", "left", "right"
     // If no, then return a direction of ""
-     ******************************************/
+    /******************************************/
     // console.log(puzzle.currentPiece);
     // console.log(blankSpace);
     
+
+    //if conditions to determine if the cube is movable
     if(puzzle.currentPiece.x == blankSpace.x){
        
         if((puzzle.currentPiece.y == blankSpace.y - 100)||(puzzle.currentPiece.y == blankSpace.y + 100)){
@@ -126,6 +144,7 @@ const puzzle = {
   }
 };
 
+//calling in initialize function to get the code going!
 puzzle.initialize();
 
 /* 
